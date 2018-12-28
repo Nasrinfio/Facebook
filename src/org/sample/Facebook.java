c8d15a9c58d67aa6746651c0bf5ba2a6b27edaee
Nasrinfio <nasrinrilwana@gmail.com>
2018-12-28 10:57:14 +0530

    created signin method

---
+++ src/org/sample/Facebook.java
@@ -0,0 +1,11 @@
+package org.sample;
+
+public class Facebook {
+
+public static void signin() {
+	System.out.println("Verfied login and working");
+}
+public static void main(String[] args) {
+	signin();
+}
+}
