ad2a4d1b2d400d53a8c2d01eeb36f2fcb41dec29
Nasrinfio <nasrinrilwana@gmail.com>
2018-12-28 11:22:53 +0530

    login changed method

--- src/org/sample/Facebook.java
+++ src/org/sample/Facebook.java
@@ -4,18 +4,24 @@ public class Facebook {
 
 	public static void verifylogin() {
 		System.out.println("verification done successfully");
+		System.out.println("verification done successfully");
+		System.out.println("verification done successfully");
 	}
 
 public static void signin() {
 	System.out.println("Verfied login and working");
+	System.out.println("verification done successfully");
 }
 
 public static void login() {
 	
 	System.out.println("verified login 2 times");
+	System.out.println("verification done successfully");
 }
 
 public static void main(String[] args) {
 	signin();
+	login();
+	verifylogin();
 }
 }
