3802111877124ace2e47f92aed55defabb9bb7dd
Nasrinfio <nasrinrilwana@gmail.com>
2018-12-28 11:19:19 +0530

    metod front

--- src/org/sample/Facebook.java
+++ src/org/sample/Facebook.java
@@ -2,6 +2,10 @@ package org.sample;
 
 public class Facebook {
 
+	public static void verifylogin() {
+		System.out.println("verification done successfully");
+	}
+
 public static void signin() {
 	System.out.println("Verfied login and working");
 }
@@ -11,9 +15,6 @@ public static void login() {
 	System.out.println("verified login 2 times");
 }
 
-public static void verifylogin() {
-	System.out.println("verification done successfully");
-}
 public static void main(String[] args) {
 	signin();
 }
